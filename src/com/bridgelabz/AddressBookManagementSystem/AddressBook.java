8092d38b1ee1abd28809040654ef10d7a4b83334
Vikash <vikashpathak041@gmail.com>
2022-06-06 15:20:55 +0530

    C:/Program Files/Git/Address-Book-Management-System start

---
+++ src/com/bridgelabz/AddressBookManagementSystem/AddressBook.java
@@ -0,0 +1,39 @@
+package com.bridgelabz.AddressBookManagementSystem;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AddressBook
+{
+    static List<ContactPerson> contactList = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
+    // method for adding contacts
+    public static void addContact() {
+        System.out.println(" Enter your first name : ");
+        String firstName = sc.nextLine();
+        System.out.println(" Enter your last name : ");
+        String lastName = sc.nextLine();
+        System.out.println(" Enter your city name : ");
+        String city = sc.nextLine();
+        System.out.println("Enter your state  : ");
+        String state = sc.nextLine();
+        System.out.println(" Enter your zip code : ");
+        long zip = sc.nextLong();
+        System.out.println(" Enter your phone number : ");
+        long phoneNumber = sc.nextLong();
+        System.out.println(" Enter your email : ");
+        String email = sc.nextLine();
+    
+        ContactPerson addressBook = new ContactPerson(firstName, lastName, email, city, state, phoneNumber, zip);
+        contactList.add(addressBook);
+    }
+    // main Method
+    public static void main(String[] args)
+    {
+        System.out.println("****** Welcome To The Address Book Program ******");
+        addContact();
+        System.out.println(contactList);
+    }
+}
---
+++ src/com/bridgelabz/AddressBookManagementSystem/ContactPerson.java
@@ -0,0 +1,85 @@
+package com.bridgelabz.AddressBookManagementSystem;
+
+
+public class ContactPerson
+{
+    private String firstName,lastName, city,state, email ;
+    private long phoneNumber,zip;
+    ContactPerson(String firstName, String lastName, String email, String city, String state,long phoneNumber, long zip)
+    {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+    public String getFirstName()
+    {
+        return firstName;
+    }
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+    public String getLastName()
+    {
+        return lastName;
+    }
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+    public String getCity()
+    {
+        return city;
+    }
+    public void setCity(String city)
+    {
+        this.city = city;
+    }
+    public String getState()
+    {
+        return state;
+    }
+    public void setState(String state)
+    {
+        this.state = state;
+    }
+    public String getEmail()
+    {
+        return email;
+    }
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+    public long getPhoneNumber()
+    {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(long phoneNumber)
+    {
+        this.phoneNumber = phoneNumber;
+    }
+    public long getZip()
+    {
+        return zip;
+    }
+    public void setZip(long zip)
+    {
+        this.zip = zip;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ContactPerson [firstName=" + firstName + ", lastName=" + lastName + ", city=" + city + ", state="
+                + state + ", email=" + email + ", phoneNumber=" + phoneNumber + ", zip=" + zip + "]";
+    }
+
+
+
+}
